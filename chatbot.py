ab0d5345338b8a3b6d36b85bd42b4df933515799
Justin <justinliu403@gmail.com>
2022-04-09 21:31:25 -0500

    idk man
    added some user recognition

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,35 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import pickle
+import yaml
+import os.path
 
 
 if __name__ == '__main__':
+    nflKnowledge = pickle.load(open('knowledgeBase.pickle', 'rb'))
+    with open(r'.\knowledgeBase.yml', 'w') as file:
+        documents = yaml.dump(nflKnowledge, file)
+    if os.path.exists("./userBase.p"):
+        user_knowledge = pickle.load(open('userBase.p', 'rb'))
+    else:
+        user_knowledge = {}
+
+
+
     chatbot = ChatBot(name ="NFL Bot")
     corpus_trainer = ChatterBotCorpusTrainer(chatbot)
-    corpus_trainer.train('chatterbot.corpus.english')
+    #corpus_trainer.train('chatterbot.corpus.english')
+    corpus_trainer.train("./knowledgeBase.yml")
     trainer = ListTrainer(chatbot)
     wants_to_talk = True
     print("Hi my name is NFL Bot! If at any time you want to stop, just enter * \nWhat is your name?")
     user_in = input()
-    print("Hello,", user_in)
-
+    if user_in in user_knowledge:
+        print("Welcome back,", user_in)
+    else:
+        print("Nice to meet you,", user_in)
+        user_knowledge[user_in] = []
 
 
     while wants_to_talk:
@@ -22,5 +39,7 @@ if __name__ == '__main__':
         else:
             response = chatbot.get_response(user_in)
             print(response)
+
+    pickle.dump(user_knowledge, open("./userBase.p", "wb"))
 
 
