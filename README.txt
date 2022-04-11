0b855389d9466a90983386be7dd5a018944e6e88
Justin <justinliu403@gmail.com>
2022-04-10 22:57:20 -0500

    final push?
    prolly not

--- README.txt
+++ README.txt
@@ -2,3 +2,8 @@ Justin Liu and Piyush Mewada
 jtl170000 and pxm170012
 4/10/2022
 Project 2: Chatbot
+
+To run this project, you need a couple of things, you need to pip install: chatterbot, chatterbot-corpus, yaml,
+spacy, and potentially some others depending on what you already have installed. You can not use any python version
+greater than 3.8.X. There is a good chance that you might not be able to get it running though, chatterbot is 
+very picky. 
--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,16 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import pickle
+import spacy
 import yaml
 import os.path
+import random
 
 
 if __name__ == '__main__':
     nflKnowledge = pickle.load(open('nflTeamKnowledge.p', 'rb'))
+    for key in nflKnowledge:
+        print(key, nflKnowledge[key])
     with open(r'.\knowledgeBase.yml', 'w') as file:
         documents = yaml.dump(nflKnowledge, file)
     if os.path.exists("./userBase.p"):
@@ -18,10 +22,11 @@ if __name__ == '__main__':
 
     chatbot = ChatBot("NFL Bot", logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
         'chatterbot.logic.BestMatch'
     ])
     corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+    print("Loading spacy, this might take a second...")
+    spacy_instance = spacy.load('en_core_web_md')
     corpus_trainer.train('chatterbot.corpus.english')
     corpus_trainer.train("./knowledgeBase.yml")
     trainer = ListTrainer(chatbot)
@@ -38,7 +43,8 @@ if __name__ == '__main__':
         user_knowledge[name] = {}
         user_in = input("Who's your favorite NFL team?")
         user_knowledge[name] = {"favorite team": user_in}
-    print("I know tons of information about the nfl, ask me about your favorite team!")
+    print("I know tons of information about the nfl, ask me about your favorite team! If referencing a team, please capitalize"
+          " their name because I use NER to recognize if a team is referenced and capitalizing helps")
 
 
     while wants_to_talk:
@@ -46,8 +52,25 @@ if __name__ == '__main__':
         if user_in == "*":
             wants_to_talk = False
         else:
-            response = chatbot.get_response(user_in)
-            print(response)
+            chatter_response_needed = True
+            doc = spacy_instance(user_in)
+            orgs = []
+            for ent in doc.ents:
+                if ent.label_ == "ORG":
+                    orgs.append(ent.text)
+            for org in orgs:
+                if org in nflKnowledge:
+                    chatter_response_needed = False
+                    if "where" in user_in.lower():
+                        print("The", org, "are based in", nflKnowledge[org][0])
+                    elif "fact" in user_in.lower() or "know" in user_in.lower():
+                        print("Here's a fun fact about the", org, ":", nflKnowledge[org][random.randint(1, 2)])
+                    else:
+                        print("Sorry, I don't know what you're trying to ask about the", org)
+
+            if chatter_response_needed:
+                response = chatbot.get_response(user_in)
+                print(response)
 
     pickle.dump(user_knowledge, open("./userBase.p", "wb"))
 
